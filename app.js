5cb36590d1284940f923f8274f4deec4f00283ff
Nicolette Furtado <ndfurtad@asu.edu>
2016-02-27 15:06:14 -0700

    rest call to get related tags

--- app.js
+++ app.js
@@ -23,7 +23,31 @@ app.post('/dashboard', function(req, res){
 });
 
 
+app.get('/test1', function(req, res){
+  // get tags related
+  var stackexchange = require('stackexchange');
+  var options = { version: 2.2 };
+  var context = new stackexchange(options);
+  var filter = {
+    pagesize:10,
+    sort: 'activity',
+    order: 'desc'
+  };
+  var relatedTags = [];
+  context.tags.related(filter, function(err, results){
+    if (err) {
+      res.render('questions1.ejs',{data:relatedTags})
+    } else {
+    for(i = 0;i<11;i++){
+      relatedTags.push(results.items[i].name);
+      console.log(relatedTags[i]);
+    }
+  }
+  }, ['javascript']);
+  // res.render('questions1.ejs',dataArray)
+});
+
+
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
-
