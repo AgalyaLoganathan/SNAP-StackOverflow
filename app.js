afa6a21164824fa158981643867eccf1d19d01e5
agalyaloganathan <agalya@Agalyas-MacBook-Pro.local>
2016-03-01 00:18:51 -0700

    wired login page to profile page; node powers WHAT TO LEARN

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ var filter = {
   order: 'asc'
 };
 
+var filter_for_tags = {
+  limit: 10,
+  pagesize: 3,
+  sort: 'activity',
+  order: 'asc'
+};
+
 var app = express();
 var router = express.Router();
 
 // all environments
-app.use(express.static(__dirname + '/public'))
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -30,96 +36,38 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get('/get-tags', function(req, res) {
-
-});
-
 app.get('/what-to-learn', function(req, res){
-  //   var relatedTags = [];
-  //   context.tags.related(filter, function(err, results){
-  //   if (err) {
-  //     // res.render('questions1.ejs',{data:relatedTags})
-  //   } else {
-  //   for(i = 0;i<11;i++){
-  //     relatedTags.push(results.items[i].name);
-  //     // console.log(relatedTags[i]);
-  //   }
-  // }
-  // }, ['javascript']);
-
-    var data = [];
-    context.tags.faq(filter, function(err, results){
+    var relatedTags = [];
+    context.tags.related(filter_for_tags, function(err, results){
     if (err) {
-        res.json(data);
-        // res.render('dashboard.ejs', {data: data})
-    } else {
-    for(i = 0; i < 11; i++) {
-        if(results.items[i].is_answered == true) {
-        // console.log(results.items[i].link);
-        // console.log(results.items[i].title);
-        var d = {'id': i,
-                'link': results.items[i].link,
-                'title': results.items[i].title,
-                'tags': results.items[i].tags};
-        data.push(d);
-        // console.log(data.length);
-        }
-    }
-}
-}, ['java']);
-});
-
-app.get('/dashboard', function(req, res){
-    console.log("inside server function");
-    var questions_to_answer = [
-    {
-      id:1,
-      question:'Test quetsion',
-      tags:['Test-tag'],
-      link:'http://stackoverflow.com/questions/18539711/input-widths-on-bootstrap-3'
-    },
-    {
-      id:2,
-      question:'Test quetsion2',
-      tags:['Test-tag'],
-      link:'http://stackoverflow.com/questions/18539711/input-widths-on-bootstrap-3'
-    },
-    {
-      id:3,
-      question:'Test quetsion3',
-      tags:['Test-tag'],
-      link:'http://stackoverflow.com/questions/18539711/input-widths-on-bootstrap-3'
-    }];
-    res.json(questions_to_answer);
-});
-app.get('/profile', function(req, res){
-    res.sendFile(__dirname + '/views/profile.html');
-});
-
-app.get('/test1', function(req, res){
-  // get tags related
-  var stackexchange = require('stackexchange');
-  var options = { version: 2.2 };
-  var context = new stackexchange(options);
-  var filter = {
-    pagesize:10,
-    sort: 'activity',
-    order: 'desc'
-  };
-  var relatedTags = [];
-  context.tags.related(filter, function(err, results){
-    if (err) {
-      res.render('questions1.ejs',{data:relatedTags})
-    } else {
-    for(i = 0;i<11;i++){
+              var data = [];
+              context.tags.faq(filter, function(err, results){
+              if (err) {
+                  res.json(data);
+              } else {
+              for(i = 0; i < 11; i++) {
+                  if(results.items[i].is_answered == true) {
+                  var d = {'id': i,
+                          'link': results.items[i].link,
+                          'question': results.items[i].title,
+                          'tags': results.items[i].tags};
+                  data.push(d);
+                  }
+              }
+          }
+          }, relatedTags);
+} else {
+    for(i = 0;i<4;i++){
+      // Fetch 3 related tags; more than this wouldn't give proper results since tags are diverse
       relatedTags.push(results.items[i].name);
-      console.log(relatedTags[i]);
     }
   }
-  }, ['javascript']);
-  // res.render('questions1.ejs',dataArray)
+  }, ['android']);
 });
 
+app.get('/profile', function(req, res){
+    res.sendFile(__dirname + '/views/dashboard.html');
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- public/js/snap.js
+++ public/js/snap.js
@@ -30,28 +30,10 @@ app.controller('WhatToAnswerController', ['$scope', function($scope) {
     }
   };
 }])
-.controller('WhatToLearnController', ['$scope', function($scope) {
-  var questions_to_learn = [
-    {
-      id:4,
-      question:'How do I compare strings in Java?',
-      tags:['java', 'string', 'equality'],
-      link:'http://stackoverflow.com/questions/513832/how-do-i-compare-strings-in-java'
-    },
-    {
-      id:5,
-      question:'What is a Null Pointer Exception, and how do I fix it?',
-      tags:['java', 'nullpointerexception'],
-      link:'http://stackoverflow.com/questions/218384/what-is-a-null-pointer-exception-and-how-do-i-fix-it'
-    },
-    {
-      id:6,
-      question:'Is Java pass-by-reference or pass-by-value?',
-      tags:['java', 'parameter-passing', 'terminology', 'pass-by-reference', 'pass-by-value'],
-      link:'http://stackoverflow.com/questions/40480/is-java-pass-by-reference-or-pass-by-value'
-    }
-  ];
-  $scope.questions_to_learn = questions_to_learn;
+.controller('WhatToLearnController', ['$scope', '$http',  function($scope, $http) {
+    $http.get('/what-to-learn').success(function(data){
+        $scope.questions_to_learn = data;
+    });
   $scope.updateCompetency = function(question_id) {
     for (var i = 0; i < $scope.questions_to_learn.length; i++) {
       if($scope.questions_to_learn[i].id == question_id) {
@@ -66,6 +48,6 @@ app.controller('WhatToAnswerController', ['$scope', function($scope) {
   $scope.competency_list = competency_list;
 }])
 .controller('LearningListController', ['$scope', function($scope) {
-  var learning_list = ['java','nullpointerexception','equality'];
+  var learning_list = ['android', 'java', 'mobile app'];
   $scope.learning_list = learning_list;
 }]);
